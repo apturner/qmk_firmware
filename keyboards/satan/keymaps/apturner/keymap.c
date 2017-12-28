7d678fd4b16e7044360bf5ff1ccfc16a30685ead
Andrew Turner <apturner@mit.edu>
2017-12-28 15:19:00 -0600

    Swapped QUI and ALT

--- keyboards/satan/keymaps/apturner/keymap.c
+++ keyboards/satan/keymaps/apturner/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-------------------------------------------------------------------------|
      * |SHIFT(CAP| Z  | X  | C  | V  | B  | N  | M  | ,  | .  | /  |   SHIFT)    |
      * |-------------------------------------------------------------------------|
-     * |CTRL | GUI | ALT |          SPACE/NUM          | ALT  | GUI  | FN2 |CTRL |
+     * |CTRL | ALT | GUI |          SPACE/NUM          | GUI  | ALT  | FN2 |CTRL |
      * `-------------------------------------------------------------------------'
      */
     [_QWERTY] = KEYMAP_ANSI(
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TAB_CAG,    KC_Q,    KC_W, KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I,    KC_O,    KC_P,  KC_LBRC,  KC_RBRC,  KC_BSLS, \
             MO(_FN1),    KC_A,    KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L, KC_SCLN,  KC_QUOT,             KC_ENT, \
         TD(TD_SHIFT),             KC_Z, KC_X, KC_C, KC_V,    KC_B, KC_N, KC_M, KC_COMM,  KC_DOT,  KC_SLSH,            KC_RSPC, \
-             KC_LCTL, KC_LGUI, KC_LALT,                   NUM_SPC,                      KC_RALT,  KC_RGUI, MO(_FN2), KC_RCTL),
+             KC_LCTL, KC_LALT, KC_LGUI,                   NUM_SPC,                      KC_RGUI,  KC_RALT, MO(_FN2), KC_RCTL),
 
     /* Keymap _FN1: Function Layer
      *  -------------------------------------------------------------------------.
