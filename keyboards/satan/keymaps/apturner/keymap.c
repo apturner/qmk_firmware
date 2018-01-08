8f083860a86c6beefbc6d3ebfa575037a6770194
Andrew Turner <apturner@mit.edu>
2018-01-08 09:38:02 -0800

    Updated mousekeys layout

--- keyboards/satan/keymaps/apturner/keymap.c
+++ keyboards/satan/keymaps/apturner/keymap.c
@@ -88,21 +88,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *  -------------------------------------------------------------------------.
      * |    | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |  DEL   |
      * |-------------------------------------------------------------------------|
-     * |      |WLUP|    |MSUP|    |BTN2|PGUP|HOME| UP |END |    |    |    |      |
+     * |      |    |WLDN|MSUP|WLUP|    |    |ACL0| UP |ACL1|ACL2|    |    |      |
      * |-------------------------------------------------------------------------|
-     * |       |WLDN|MSLF|MSDN|MSRT|BTN1|PGDN|LEFT|DOWN|RGHT|INS | `  |          |
+     * |       |BTN2|MSLF|MSDN|MSRT|    |BTN1|LEFT|DOWN|RGHT|    | `  |          |
      * |-------------------------------------------------------------------------|
-     * |         |    |    |    |    |    |ACL0|ACL1|ACL2|    |    |             |
+     * |         |    |    |    |    |HOME|PGUP|PGDN|END |INS |    |             |
      * |-------------------------------------------------------------------------|
      * |     |     |     |                             |      |      |     |     |
      * `-------------------------------------------------------------------------'
      */
     [_FN1] = KEYMAP_ANSI(
-        ____, KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, \
-        ____,  KC_WH_U,    ____, KC_MS_U,    ____, KC_BTN2, KC_PGUP, KC_HOME,   KC_UP,  KC_END,   ____,   ____,   ____,   ____, \
-        ____,  KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS, KC_GRV,           ____, \
-        ____,              ____,    ____,    ____,    ____,    ____, KC_ACL0, KC_ACL1, KC_ACL2,   ____,   ____,           ____, \
-        ____,  ____,       ____,                               ____,                              ____,   ____,   ____,  ____),
+        ____,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, \
+        ____,    ____, KC_WH_D, KC_MS_U, KC_WH_U,  ____,    ____, KC_ACL0,   KC_UP, KC_ACL1, KC_ACL2,   ____,   ____,   ____, \
+        ____, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R,  ____, KC_BTN1, KC_LEFT, KC_DOWN, KC_RGHT,    ____, KC_GRV,           ____, \
+        ____,             ____,    ____,    ____,  ____, KC_HOME, KC_PGUP, KC_PGDN,  KC_END,  KC_INS,   ____,           ____, \
+        ____,    ____,    ____,                             ____,                               ____,   ____,   ____,  ____),
 
     /* Keymap _NUM: Keypad Layer
      *  -------------------------------------------------------------------------.
