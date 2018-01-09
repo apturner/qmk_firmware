297e87c0031102fc9d1f11b74a370c31362bb42e
Andrew Turner <apturner@mit.edu>
2018-01-09 09:25:08 -0800

    Adjusted mousekeys layout and settings

--- keyboards/satan/keymaps/apturner/config.h
+++ keyboards/satan/keymaps/apturner/config.h
@@ -100,7 +100,7 @@ These options are also useful to firmware size reduction.
 #define MOUSEKEY_DELAY             300
 #define MOUSEKEY_INTERVAL          50
 #define MOUSEKEY_MAX_SPEED         8
-#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_TIME_TO_MAX       10
 #define MOUSEKEY_WHEEL_MAX_SPEED   5
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
