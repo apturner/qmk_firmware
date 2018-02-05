e525d3778f5907698c3a974e38fb1ba22a4f4965
Andrew Turner <apturner@mit.edu>
2018-02-05 16:15:39 -0500

    Mousekeys updates

--- keyboards/ergodox_ez/keymaps/apturner/config.h
+++ keyboards/ergodox_ez/keymaps/apturner/config.h
@@ -44,7 +44,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_INTERVAL          15
 #define MOUSEKEY_MAX_SPEED         8
 #define MOUSEKEY_TIME_TO_MAX       10
-#define MOUSEKEY_WHEEL_MAX_SPEED   5
+#define MOUSEKEY_WHEEL_MAX_SPEED   4
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 10
 #define MOUSEKEY_WHEEL_DELAY       0
 
