68dcd3586af5e10896d4f5fba4fb354acc82e4f9
Andrew Turner <apturner@mit.edu>
2018-02-05 16:48:00 -0500

    Mousekeys updates

--- keyboards/satan/keymaps/apturner/config.h
+++ keyboards/satan/keymaps/apturner/config.h
@@ -99,7 +99,7 @@ These options are also useful to firmware size reduction.
 
 #define MOUSEKEY_DELAY             200
 #define MOUSEKEY_INTERVAL          15
-#define MOUSEKEY_MAX_SPEED         7
+#define MOUSEKEY_MAX_SPEED         6
 #define MOUSEKEY_TIME_TO_MAX       10
 #define MOUSEKEY_WHEEL_MAX_SPEED   4
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 10
