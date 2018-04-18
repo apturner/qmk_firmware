737aeb10bd4bbbb876a60cfc75a07e5cab593d84
Andrew Patrick Turner <apturner@mit.edu>
2018-04-18 12:29:53 -0400

    Ergodox update

--- keyboards/ergodox_ez/keymaps/apturner/config.h
+++ keyboards/ergodox_ez/keymaps/apturner/config.h
@@ -44,9 +44,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_INTERVAL          15
 #define MOUSEKEY_MAX_SPEED         6
 #define MOUSEKEY_TIME_TO_MAX       10
-#define MOUSEKEY_WHEEL_MAX_SPEED   4
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 10
-#define MOUSEKEY_WHEEL_DELAY       0
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+// #define MOUSEKEY_WHEEL_DELAY       0
 
 #define TAPPING_TOGGLE  1
 
