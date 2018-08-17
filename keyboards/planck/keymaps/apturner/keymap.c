a2c920c0acf3815f7e1cdb7a0613817896791609
Andrew Patrick Turner <apturner@mit.edu>
2018-08-17 15:09:37 -0400

    Changed lower and raise layouts

--- keyboards/planck/keymaps/apturner/keymap.c
+++ keyboards/planck/keymaps/apturner/keymap.c
@@ -17,6 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "planck.h"
 #include "action_layer.h"
+#include "keymap_steno.h"
 #include "apturner.h"
 
 extern keymap_config_t keymap_config;
@@ -85,34 +86,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
 	 * | CAG  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  -   |  =   |  [   |  ]   |  \   |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |      |      |      |      |      |
+	 * |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |  _   |  +   |  {   |  }   |      |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
 	 * | Del  |      |      |      |      |      |      |      | Mute | Prev | Next | Play |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_LOWER] = {
-		{KC_GRV, KC_1,  KC_2,  KC_3,  KC_4,   KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0,    KC_BSPC},
-		{CAG,    KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,  KC_F6,  KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS},
-		{____,   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, ____,    ____,   ____,    ____,       ____},
-		{KC_DEL, ____,  ____,  ____,  ____,   ____,   ____,   ____,    MUTE,   PREV,    NEXT,    KC_MPLY}
+		{KC_GRV, KC_1,  KC_2,  KC_3,  KC_4,   KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
+		{CAG,    KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,  KC_F6,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+		{____,   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,    ____},
+		{KC_DEL, ____,  ____,  ____,  ____,   ____,   ____,   ____,    MUTE,    PREV,    NEXT,    KC_MPLY}
 	},
 
 	/* Raise
 	 * ,-----------------------------------------------------------------------------------.
 	 * |  ~   |  !   |  @   |  #   |  $   |  %   |  ^   |  &   |  *   |  (   |  )   | Bksp |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | Meh  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  _   |  +   |  {   |  }   |  |   |
+	 * | Meh  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  -   |  =   |  [   |  ]   |  |   |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |      |      |      |      |      |
+	 * |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |  _   |  +   |  {   |  }   |      |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | Del  |      |      |      |      |      |      |      |      |      |      |      |
+	 * | Del  |      |      |      |      |      |      |      | Mute | Prev | Next | Play |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_RAISE] = {
 		{KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-		{KC_MEH,  KC_F1,   KC_F2, KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-		{____,    KC_F7,   KC_F8, KC_F9,   KC_F10, KC_F11,  KC_F12,  ____,    ____,    ____,    ____,       ____},
-		{KC_DEL,  ____,    ____,  ____,    ____,   ____,    ____,    ____,    ____,    ____,    ____,       ____}
+		{KC_MEH,  KC_F1,   KC_F2, KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE},
+		{____,    KC_F7,   KC_F8, KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,    ____},
+		{KC_DEL,  ____,    ____,  ____,    ____,   ____,    ____,    ____,    MUTE,    PREV,    NEXT,    KC_MPLY}
 	},
 
 	/* Adjust (Lower + Raise)
@@ -121,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
 	 * | SAG  | Poly |MusMod|AudOn |AudOff|      |      |      |      |      |      |      |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |      |Voice-|Voice+|MusTog|      |      |      |      |      |      |      |QWERTY|
+	 * |      |Voice-|Voice+|MusTog|      |      |      |      |      |      |      |PLOVER|
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
 	 * |ToggOS|      |      |      |      |      |      |      | Vol- | Vol+ |Brite-|Brite+|
 	 * `-----------------------------------------------------------------------------------'
@@ -129,27 +130,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ADJUST] = {
 		{KC_MAKE, ____,   ____,   ____,   ____,   ____, ____, ____, ____, ____, ____,    KC_SLEP},
 		{SAG,     POLY,   MU_MOD, AU_ON,  AU_OFF, ____, ____, ____, ____, ____, ____,       ____},
-		{____,    MUV_DE, MUV_IN, MU_TOG, ____,   ____, ____, ____, ____, ____, ____,    QWERTY },
+		{____,    MUV_DE, MUV_IN, MU_TOG, ____,   ____, ____, ____, ____, ____, ____,    PLOVER },
 		{TOGG_OS, ____,   ____,   ____,   ____,   ____, ____, ____, VOLD, VOLU, MAC_DIM, MAC_BRI}
 	},
 
 	/* Plover layer (http://opensteno.org)
 	 * ,-----------------------------------------------------------------------------------.
-	 * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
+	 * |  #   |  #   |  #   |  #   |  #   |  #   |  #   |  #   |  #   |  #   |  #   |  #   |
+	 * |------+------+------+------+------+-------------+------+------+------+------+------|
+	 * |  FN  |  S   |  T   |  P   |  H   |  *   |  *   |  F   |  P   |  L   |  T   |  D   |
+	 * |------+------+------+------+------+------|------+------+------+------+------+------|
+	 * |      |  S   |  K   |  W   |  R   |  *   |  *   |  R   |  B   |  G   |  S   |  Z   |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |      |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
-	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
-	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * | Exit |      |      |   A  |   O  |      |      |   E  |   U  |      |      |      |
+	 * | Exit |      |      |      |  A   |  O   |  E   |  U   |      |  PWR | RES1 | RES2 |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 
 	[_PLOVER] = {
-		{KC_1,    KC_1, KC_1, KC_1, KC_1, KC_1, KC_1, KC_1, KC_1, KC_1, KC_1,    KC_1},
-		{XXXX,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,    KC_LBRC},
-		{XXXX,    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
-		{EXT_PLV, XXXX, XXXX, KC_C, KC_V, XXXX, XXXX, KC_N, KC_M, XXXX, XXXX,    XXXX}
+		{STN_N1,  STN_N2, STN_N3, STN_N4, STN_N5, STN_N6,  STN_N7,  STN_N8, STN_N9, STN_NA,  STN_NB,  STN_NC },
+		{STN_FN,  STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1, STN_ST3, STN_FR, STN_PR, STN_LR,  STN_TR,  STN_DR },
+		{XXXX,    STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2, STN_ST4, STN_RR, STN_BR, STN_GR,  STN_SR,  STN_ZR },
+		{EXT_PLV, XXXX,   XXXX,   XXXX,   STN_A,  STN_O,   STN_E,   STN_U,  XXXX,   STN_PWR, STN_RE1, STN_RE2}
 	}
 };
 
@@ -160,6 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 
 void matrix_init_keymap(void) {
+	steno_set_mode(STENO_MODE_GEMINI);
 	set_polyphony_rate(5);
 	polyphony_on = true;
 }
