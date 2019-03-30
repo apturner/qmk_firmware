06d9773f2da94774f20f3b3c1c47744a78671bf3
Andrew Patrick Turner <apturner@mit.edu>
2019-03-29 22:54:04 -0400

    Added lighting in game layer

--- keyboards/satan/keymaps/apturner/keymap.c
+++ keyboards/satan/keymaps/apturner/keymap.c
@@ -148,11 +148,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-------------------------------------------------------------------------'
      */
     [_GAME] = LAYOUT_60_ansi(
-            KC_GESC,    KC_1,    KC_2, KC_3, KC_4, KC_5,    KC_6, KC_7, KC_8,    KC_9,    KC_0,  KC_MINS,   KC_EQL,  KC_BSPC, \
-             KC_TAB,    KC_Q,    KC_W, KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I,    KC_O,    KC_P,  KC_LBRC,  KC_RBRC,  KC_BSLS, \
-        MO(_GAMEFN),    KC_A,    KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L, KC_SCLN,  KC_QUOT,             KC_ENT, \
-            KC_LSFT,             KC_Z, KC_X, KC_C, KC_V,    KC_B, KC_N, KC_M, KC_COMM,  KC_DOT,  KC_SLSH,            KC_RSFT, \
-            KC_LCTL, KC_LALT, KC_LGUI,                    KC_SPC,                      KC_RGUI,  KC_RALT, EXT_GAME, KC_RCTL),
+            KC_GESC,    KC_1,    KC_2, KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,    KC_0,  KC_MINS,   KC_EQL,  KC_BSPC, \
+             KC_TAB,    KC_Q,    KC_W, KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,  KC_LBRC,  KC_RBRC,  KC_BSLS, \
+        MO(_GAMEFN),    KC_A,    KC_S, KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L, KC_SCLN,  KC_QUOT,             KC_ENT, \
+            KC_LSFT,             KC_Z, KC_X, KC_C, KC_V,   KC_B, KC_N, KC_M, KC_COMM,  KC_DOT,  KC_SLSH,            KC_RSFT, \
+            KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                      KC_RGUI,  KC_RALT, EXT_GAME, KC_RCTL),
 
     /* Keymap _GAMEFN: Game Function Layer
      *  -------------------------------------------------------------------------.
@@ -250,6 +250,18 @@ void led_set_keymap(uint8_t usb_led) {
             PORTB |= (1<<2);
         }
     #endif
+}
+
+uint32_t layer_state_set_keymap (uint32_t state) {
+    switch (biton32(state)) {
+        case _GAME:
+            backlight_enable();
+            break;
+        case _QWERTY:
+            backlight_disable();
+            break;
+    }
+    return state;
 }
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
